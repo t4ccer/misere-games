bb897af2ca065d453043f5da677196fc91b36fa7
t4ccer <t4ccer@gmail.com>
2026-07-01 11:24:20 -0230

    feat: add page for "On the general dead-ending universe of partizan games"
    Very incomplete for now

--- CombinatorialGames.lean
+++ CombinatorialGames.lean
@@ -16,6 +16,7 @@ public import CombinatorialGames.GameForm.Birthday
 public import CombinatorialGames.GameForm.Special
 public import CombinatorialGames.GameGraph
 public import CombinatorialGames.Literature.OnSumsOfPFreeFormsUnderMiserePlay
+public import CombinatorialGames.Literature.OnTheGeneralDeadEndingUniverseOfPartizanGames
 public import CombinatorialGames.Mathlib.NatOrdinal
 public import CombinatorialGames.Mathlib.SimpleGraph
 public import CombinatorialGames.Mathlib.Small
